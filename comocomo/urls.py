5352119364e057ad2fad0834c4b29e1c89ffe766
Andrés Moya <andres.moya@kaleidos.net>
2013-07-16 16:16:20 +0200

    First static page for calendar week

--- comocomo/urls.py
+++ comocomo/urls.py
@@ -1,15 +1,24 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+
+from comocomo.views import *
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'comocomo.views.home', name='home'),
     # url(r'^comocomo/', include('comocomo.foo.urls')),
+    url(r'^week/$', WeekView.as_view(), name='week'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+
---
+++ comocomo/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.views.generic.base import TemplateView
+
+class WeekView(TemplateView):
+
+    template_name = 'comocomo/week.html'
+
