8bb5bed85569e6115854fc7e13bb77f4e013a70f
Mario Garcia <mario.ggar@gmail.com>
2018-12-17 14:53:15 +0100

    Add bedca first level groups

--- comocomo-spiders/bedca/level_one.py
+++ comocomo-spiders/bedca/level_one.py
@@ -1,5 +1,7 @@
 import time
+import json
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
@@ -16,6 +18,19 @@ try:
     groups = driver.find_element_by_xpath('//a[@id="General"]')
     groups.click()
     time.sleep(5)
+
+    # dairy groups
+    # import pdb; pdb.set_trace()
+    select = driver.find_element_by_xpath('//select[@id="fglist"]')
+    options = select.find_elements_by_tag_name("option")
+
+    # looping through options
+    groups = [{'group_name': option.text,
+               'group_id': option.get_attribute('value')} for option in options]
+
+    with open("groups.json", "w") as write_file:
+        json.dump(groups, write_file)
+
 except:
     print("WTF!")
 finally:
