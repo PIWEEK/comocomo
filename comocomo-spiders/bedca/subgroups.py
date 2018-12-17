5c5720624a7d3cfc3f8a95966cb3e0904271687c
Mario Garcia <mario.ggar@gmail.com>
2018-12-17 23:16:11 +0100

    Add changes to add subgroups

--- comocomo-spiders/bedca/subgroups.py
+++ comocomo-spiders/bedca/subgroups.py
@@ -1,26 +1,53 @@
+import time
 import json
+
 from common import navigate_to_groups
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 def serialize_subgroups_to_json():
+    subgroups = []
+
     try:
         driver = webdriver.Chrome()
         options = navigate_to_groups(driver)
+        driver.implicitly_wait(10)
 
-        for option in options:
-            option.select()
-            submit = driver.find_element_by_xpath('//input[@type="button" && value="Consultar"]')
+        for idx, option in enumerate(options):
+            index = idx + 1
+            option_name = option.text
+
+            sleeping(5, f'next iteration {index}')
+            select = Select(driver.find_element_by_xpath('//select[@id="fglist"]'))
+            select.select_by_index(index)
+
+            submit = driver.find_element_by_xpath('//input[@type="button" and @value="Consultar"]')
             submit.click()
-            food_elements = driver.find_element_by_xpath('//tr[starts-with(@class, "row-")')
-            for food in food_elements:
-                name = food.find_element_by_xpath('//td[@id="foodname"]/a').text
-                print(name)
-                print('end loop')
-            break
+            sleeping(5, 'next group elements')
 
-        print('end')
+            items = driver.find_elements_by_xpath('//tr[starts-with(@class, "row-")]')
+            subgroup = create_subgroup(option_name, items)
+            subgroups.append(subgroup)
+
+            options = navigate_to_groups(driver)
     finally:
         driver.close()
 
+    return subgroups
+
+def sleeping(seconds, message):
+    print(f"sleeping... {message}")
+    time.sleep(seconds)
+
+def create_subgroup(option, food_items):
+    print(f"creating group {option}")
+    import pdb; pdb.set_trace()
+    food_list = [{'id': food.find_element_by_xpath('/td[position()=1]/a').text,
+                  'name': food.find_element_by_xpath('/td[position()=2]/a').text,
+                  'spanish_name': food.find_element_by_xpath('/td[position()=3]/a').text} for food in food_items]
+
+    subgroup = {'name': option, 'items': food_list}
+    print(subgroup)
+    return subgroup
 
 serialize_subgroups_to_json()
